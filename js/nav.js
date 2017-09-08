5fe3c4b00335ef14f491021dc01a3bcf7814b4d2
Alternative Education <admin@alternativeeducationindia.net>
2017-09-08 14:49:15 +0530

    Massive Changes
    Shifted to proper jekkyl pages fomart
    changed nav and footer to includes folder
    removed articles and parent stories page to create blog post page
    removed unnecessary javasctipt files
    removed unecessarry css files
    removed unused html pages

--- js/nav.js
+++
@@ -1,6 +0,0 @@
-$('.hmburger').on('click', function(e) {
-  // Prevent link from jumping to the top of the page
-  e.preventDefault();
-  // If menu is already showing, slide it up. Otherwise, slide it down.
-  $('.menu').toggleClass('open');
-});
